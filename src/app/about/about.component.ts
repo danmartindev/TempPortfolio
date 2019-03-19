e8097407d1e2a1265eb001f5b8b21217b4e59d5d
Dan Martin <danmartindev@gmail.com>
2019-03-19 14:30:21 -0400

    added fadein

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { fadeIn } from '../animations';
 
 @Component({
   selector: 'about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
+  animations: [
+    fadeIn
+  ]
 })
 export class AboutComponent implements OnInit {
 
